8796590ced1c7ec73830b7c1942231c6d607d210
guilhermetravinski <guitrafer@gmail.com>
2022-10-20 21:48:07 -0300

    Create new task component

--- to-do-list-app/src/Components/NewTask/index.tsx
+++ to-do-list-app/src/Components/NewTask/index.tsx
@@ -1,8 +1,43 @@
-export function NewTask() {
+import { PlusCircle } from 'phosphor-react'
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react'
+import styles from './NewTask.module.css'
+
+interface NewTaskProps {
+  onCreateNewTask: (title: string) => void
+}
+
+export function NewTask({ onCreateNewTask }: NewTaskProps) {
+  const [newTaskText, setNewTaskText] = useState('')
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault()
+    onCreateNewTask(newTaskText)
+    setNewTaskText('')
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('')
+    setNewTaskText(event.target.value)
+  }
+
+  function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('Este campo é obrigatório!')
+  }
+
+  const isNewTaskEmpty = newTaskText.length === 0
   return (
-    <form>
-      <input />
-      <button />
+    <form className={styles.addTaskContainer} onSubmit={handleCreateNewTask}>
+      <input
+        className={styles.input}
+        type="text"
+        placeholder="Adicione uma nova tarefa"
+        value={newTaskText}
+        onChange={handleNewTaskChange}
+        onInvalid={handleNewTaskInvalid}
+      />
+      <button type="submit" className={styles.button} disabled={isNewTaskEmpty}>
+        Criar <PlusCircle size={20} />
+      </button>
     </form>
   )
 }
