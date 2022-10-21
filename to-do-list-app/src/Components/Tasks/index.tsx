b97ad543bfbed82878f082208ccc707a275e2394
guilhermetravinski <guitrafer@gmail.com>
2022-10-20 22:08:00 -0300

    Create core funcions on App.tsx

--- to-do-list-app/src/Components/Tasks/index.tsx
+++ to-do-list-app/src/Components/Tasks/index.tsx
@@ -1,9 +1,41 @@
-import { TaskConainer } from './styles'
+import styles from './Tasks.module.css'
 
-export function Tasks() {
+type Task = {
+  id: string
+  title: string
+  isCompleted: boolean
+}
+
+interface TasksProps {
+  tasks: Task[]
+  onCompleteTask: (taskId: string) => void
+  onRemoveTask: (taskId: string) => void
+}
+
+export function Tasks({ tasks, onCompleteTask, onRemoveTask }: TasksProps) {
+  const totalTaskCreated = 0
+  const totalTaskDone = tasks.reduce(
+    (acc, cur) => acc + Number(cur.isCompleted),
+    0,
+  )
+
   return (
-    <TaskConainer>
-      <span>tasks</span>
-    </TaskConainer>
+    <div className={styles.taskContainer}>
+      <div className={styles.taskInfoContainer}>
+        <p className={styles.infoTextCreated}>
+          Tarefas criadas <span>{totalTaskCreated}</span>
+        </p>
+        <p className={styles.infoTextDone}>
+          Concluídas{' '}
+          <span className={styles.taskInfoCounter}>
+            <>
+              {totalTaskCreated === 0
+                ? totalTaskCreated
+                : `${totalTaskDone} de ${totalTaskCreated}`}
+            </>
+          </span>
+        </p>
+      </div>
+    </div>
   )
 }
